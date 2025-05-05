687f702581889a5e8ded09f23a92101a73b50753
Shahriarnz14 <shahriar.nz14@gmail.com>
2025-05-05 12:35:42 -0400

    rearranged scripts of run files

---
+++ scripts/run_meta_learners_impute.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Usage:
+# ./run_meta_learners_impute.sh t_learner 5000
+
+# Get arguments or set defaults
+META_LEARNER=${1:-t_learner}
+TRAIN_SIZE=${2:-5000}
+
+# Constants
+REPEATS=10
+TEST_SIZE=5000
+IMPUTED_PATH="synthetic_data/imputed_times_lookup.pkl"
+IMPUTE_METHODS=("Pseudo_obs" "Margin" "IPCW-T")
+
+# Loop through each method and run
+for METHOD in "${IMPUTE_METHODS[@]}"
+do
+    echo "Running with imputation method: $METHOD, meta-learner: $META_LEARNER, train size: $TRAIN_SIZE"
+    python run_meta_learner_impute.py \
+        --num_repeats $REPEATS \
+        --train_size $TRAIN_SIZE \
+        --test_size $TEST_SIZE \
+        --meta_learner $META_LEARNER \
+        --impute_method $METHOD \
+        --load_imputed \
+        --imputed_path $IMPUTED_PATH
+done
---
+++ scripts/run_meta_learners_survival.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Usage:
+# ./run_meta_learners_survival.sh t_learner_survival 5000
+
+# Get arguments or set defaults
+META_LEARNER=${1:-t_learner_survival}
+TRAIN_SIZE=${2:-5000}
+
+# Constants
+REPEATS=10
+TEST_SIZE=5000
+NUM_MATCHES=5
+SURVIVAL_METHODS=("RandomSurvivalForest" "DeepSurv" "DeepHit")
+
+# Loop through each method and run
+for SURVIVAL_METHOD in "${SURVIVAL_METHODS[@]}"
+do
+    echo "Running with survival method: $METHOD, meta-learner: $META_LEARNER, train size: $TRAIN_SIZE"
+    python run_meta_learner_survival.py \
+        --num_repeats $REPEATS \
+        --train_size $TRAIN_SIZE \
+        --test_size $TEST_SIZE \
+        --meta_learner $META_LEARNER \
+        --base_survival_model $SURVIVAL_METHOD \
+        --num_matches $NUM_MATCHES
+done
